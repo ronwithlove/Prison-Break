4bb054e5f1a8e87b57e840bcfc8c3a0cced40dce
ronwithlove <33899958@qq.com>
2015-11-02 23:10:00 +0800

    Version 1.3.1 +Collider Scripts
    补上两个碰撞器脚本

---
+++ PlayerSmallCollider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSmallCollider : MonoBehaviour {
+	
+	private PlayerAnimation playerAnimation;
+	
+	void Awake(){
+		playerAnimation=GameObject.FindGameObjectWithTag(Tags.player).GetComponent<PlayerAnimation>();//获取Player下面的PlayerAnimation
+	}
+	
+	void OnTriggerEnter(Collider other){
+		if(other.tag==Tags.obstacles && GameController.gameState==GameState.Playing&&playerAnimation.animState==AnimationState.Slide){
+			GameController.gameState=GameState.End;
+		}
+	}
+}
