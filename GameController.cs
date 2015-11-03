daaac651a62c2cd54bbc987b540f0182b136a414
ronwithlove <33899958@qq.com>
2015-11-03 15:45:38 +0800

    Version 1.50 Game Over
    增加游戏结束界面，点击会重新开始

--- GameController.cs
+++ GameController.cs
@@ -10,6 +10,7 @@ public class GameController : MonoBehaviour {
 
 	public static GameState gameState=GameState.Menu;
 	public GameObject tapToStart;
+	public GameObject gameOver;
 
 	void Update(){
 		if( gameState==GameState.Menu){
@@ -17,6 +18,16 @@ public class GameController : MonoBehaviour {
 				gameState=GameState.Playing;
 				tapToStart.SetActive(false);
 			}
+		}
+		if(gameState==GameState.End){
+			gameOver.SetActive(true);
+
+			if(Input.GetMouseButtonDown(0)){
+				gameState=GameState.Menu;//因为这是一个静态变量，所以Scene也是公用的，值光写下面的是没用的，要把State重设一下
+				Application.LoadLevel (0);
+
+			}
+
 		}
 	}
 
