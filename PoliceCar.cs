d02097e7c3c23bee669b5669911ff7e8295dfbf5
ronwithlove <33899958@qq.com>
2015-11-03 15:12:44 +0800

    Version 1.40 Added Police Car
    增加了警察追击，警灯旋转，警车追上后刹车声音

---
+++ PoliceCar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoliceCar : MonoBehaviour {
+	private bool isPlay=false;
+
+	void Update () {
+		if(GameController.gameState==GameState.End){
+			PlayTireSquealSound();
+		}
+	}
+
+	private void PlayTireSquealSound(){
+		if(isPlay==false&&GetComponent<AudioSource>().isPlaying==false){
+			GetComponent<AudioSource>().Play();
+			isPlay=true;
+		}
+	}
+
+}
---
+++ siren.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class siren : MonoBehaviour {
+
+	public float rotateSpeed=180;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up*Time.deltaTime*rotateSpeed);
+	}
+}
